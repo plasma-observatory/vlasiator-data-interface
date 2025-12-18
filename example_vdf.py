704c51b0b849c84da2f43b2d61e67529022054e4
Markku Alho <markku.alho@helsinki.fi>
2025-12-18 14:12:00 +0200

    Added dense array and particle export to .npz, as well as a crude dense array downsample to have a bit more wieldy toy exports

--- example_vdf.py
+++ example_vdf.py
@@ -13,9 +13,18 @@ print(cellid)
 pt.plot.plot_vdf(vlsvobj=f, cellids=[cellid], xz=1, outputdir="./", title="Projected VDF")
 
 distr, edges = f.read_velocity_distribution_dense(cellid)
-ex = edges[0]
-ey = edges[1]
-ez = edges[2]
+
+downsample=True
+
+if downsample:
+    distr = distr[::2,::2,::2]
+    ex = edges[0][::2]
+    ey = edges[1][::2]
+    ez = edges[2][::2]
+else:
+    ex = edges[0][::2]
+    ey = edges[1][::2]
+    ez = edges[2][::2]
 
 pc = plt.pcolor(ex,ez, np.squeeze(distr[:,ey.size//2,:]).T, norm="log", vmin=7.0630006e-16, )
 plt.colorbar()
@@ -43,14 +52,14 @@ lY = Y.reshape((-1))
 lZ = Z.reshape((-1))
 
 ldistr = distr.reshape((-1))
-plt.hist(distr.reshape((-1)), bins=1000, log=True)
+plt.hist(distr.reshape((-1)), bins=30, log=True)
 plt.yscale("log")
 plt.title("Histogram of v-space sample weights")
 plt.xlabel("$f(v)$")
 plt.savefig("vdf-hist.png")
 plt.close()
 
-samples = 10000000
+samples = 100000
 
 choice = np.random.choice(lX.size, size=samples, p =ldistr/np.sum(ldistr))
 particle_vs = np.random.random_sample(size=(samples,3))
@@ -62,12 +71,19 @@ ldZ = dZ.reshape((-1))
 particle_vs *= np.stack((ldX[choice],ldY[choice],ldZ[choice]),axis=1) # scale by v-cell size
 particle_vs += np.stack((lX[choice],lY[choice],lZ[choice]), axis=1) # offset by chosen bin edges
 
-nbins=1000
-plt.hist2d(particle_vs[:,0], particle_vs[:,2], bins=nbins, norm="log")
+nbins=30
+h,_,_,_ = plt.hist2d(particle_vs[:,0], particle_vs[:,2], bins=nbins, norm="log")
+
+plt.clim(vmin=np.median(h)/3)
 plt.axis("equal")
 plt.colorbar(label="counts")
 plt.xlabel("$v_x / \mathrm{m}\,\mathrm{s}^{-1}$")
 plt.ylabel("$v_z / \mathrm{m}\,\mathrm{s}^{-1}$")
 plt.title(f"Counts in $(v_x,v_z)$ ({samples} samples, {nbins}$^2$ bins)")
-plt.savefig("testhist.png")
+plt.savefig("VDF_sampled_histogram.png")
 
+print(distr.shape)
+
+np.savez("VDF_example",allow_pickle=False, dense_array=distr, dense_array_edges_x=ex, dense_array_edges_y=ey, dense_array_edges_z=ez, sampled_particles=particle_vs)
+
+np.savez("VDF_example_particles",allow_pickle=False, sampled_particles=particle_vs)
